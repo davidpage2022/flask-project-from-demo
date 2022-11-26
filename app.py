4172a59a82d940a74514be83b4cbfa8ba3581e89
David C. Page <davidpage876@gmail.com>
2022-11-26 11:43:46 +1000

    Convert celsius to fahrenheit using Flask routes

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def celsius_to_fahrenheit(celsius):
+    """Convert celsius to fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return "<h1>Hello World :)</h1>"
@@ -12,6 +17,14 @@ def hello_world():  # put application's code here
 @app.route('/greet/<name>')
 def greet(name=""):
     return f"Hello {name}"
+
+
+@app.route('/fahrenheit/<celsius>')
+def fahrenheit(celsius=""):
+    try:
+        return str(celsius_to_fahrenheit(float(celsius)))
+    except ValueError:
+        return ""
 
 
 if __name__ == '__main__':
