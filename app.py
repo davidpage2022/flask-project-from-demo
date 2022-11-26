dbee6b9fd149b263ab6c64ec4a8398912b702805
David C. Page <davidpage876@gmail.com>
2022-11-26 11:36:10 +1000

    Complete basic Flask exercises

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return "<h1>Hello World :)</h1>"
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
 
 
 if __name__ == '__main__':
